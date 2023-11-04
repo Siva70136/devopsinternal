b54dee76cbc09d3311d8e9ed0b3049a6e0912cc8
Siva70136 <94431863+Siva70136@users.noreply.github.com>
2023-11-04 09:38:44 +0530

    Update UserValidationTest.java

--- src/test/java/com/cse/devops/UserValidationTest.java
+++ src/test/java/com/cse/devops/UserValidationTest.java
@@ -15,25 +15,20 @@ public class UserValidationTest {
 	public void testCase2()
 	{
 		UserValidation uv=new UserValidation();
-		Assert.assertEquals(false,uv.check("Srinu", "Srinu@12345"));
+		Assert.assertEquals(false,uv.check("Siva", "Siva@12345"));
 	}
 	@Test
 	public void testCase3()
 	{
 		UserValidation uv=new UserValidation();
-		Assert.assertEquals(false,uv.check("srinu", "srinu@12345"));
+		Assert.assertEquals(false,uv.check("siva", "siva@45"));
 	}
 	@Test
 	public void testCase4()
 	{
 		UserValidation uv=new UserValidation();
-		Assert.assertEquals(false,uv.check("SRINU", "SRINU@12345"));
+		Assert.assertEquals(false,uv.check("SIVA", "SIVA@12345"));
 	}
-	@Test
-	public void testCase5()
-	{
-		UserValidation uv=new UserValidation();
-		Assert.assertEquals(true,uv.check("srinu", "srinu212345"));
-	}
+	
 
 }
